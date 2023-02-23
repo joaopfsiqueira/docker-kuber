5807ec594a7e91ef5c8830806369cf767fdd5b3d
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-02-23 09:56:25 -0300

    feat: criando rota para retornar na imagem

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ require('dotenv').config()
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`));
