8a757470ba3d678599abcda2a3793a6bb4b16ed7
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-02-23 10:03:53 -0300

    feat: subindo readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Docker Kuber
+
+- Repositório criado para criar uma aplicação que vai ser dockerizada e controlada à partir kubernetes.
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config()
 
-const app = express();
+const app = express();  
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
