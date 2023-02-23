871d8627268908e8cd6447dbd0018aad3532a075
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-02-23 09:55:25 -0300

    fix: express

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from 'express';
+const express = require('express');
 require('dotenv').config()
 
 const app = express();
 
-app.listen(process.env.PORT, () => console.log(`Example app listening on port ${port}!`));
+app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`));
